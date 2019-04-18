63f29ac69a8e8fd26e3d83ea59f198bf86250487
Jens Henneberg <hens.jenneberg@gmail.com>
2019-04-18 16:53:52 +0200

    Add some optimizations
    [spoiler]The code won't run faster[/spoiler]

--- benchmarks/FizzBuzzBench.php
+++ benchmarks/FizzBuzzBench.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use HJenneberg\FizzBuzz\FizzBuzzOne;
+use HJenneberg\FizzBuzz\FizzBuzzOneBetter;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 
@@ -13,10 +14,18 @@ class FizzBuzzBench
 {
     /**
      * @Revs(1000)
-     * @Iterations(5)
+     * @Iterations(10)
      */
-    public function benchGet()
+    public function benchOne()
     {
-        FizzBuzzOne::get(1000);
+        FizzBuzzOne::get(10000);
+    }
+    /**
+     * @Revs(1000)
+     * @Iterations(10)
+     */
+    public function benchOneBetter()
+    {
+        FizzBuzzOneBetter::get(10000);
     }
 }
